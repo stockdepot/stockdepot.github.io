098ba96232bda486231e866191a50e02afcb7edb
stockdepot <itcwangfeng@hotmail.com>
2021-06-12 19:17:31 -0500

    Publish

---
+++ precache-manifest.25869908b883b8b14af8cdfe94fcb131.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def4cc3e0dff88a8a0fe47effd040b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247edd798df1b8ea6e6e",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9ddf3db0ee11775bede5",
+    "url": "/static/js/2.121357b8.chunk.js"
+  },
+  {
+    "revision": "e49141e7904bd032c2523726fff129e4",
+    "url": "/static/js/2.121357b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247edd798df1b8ea6e6e",
+    "url": "/static/js/main.f9edc156.chunk.js"
+  },
+  {
+    "revision": "f66f7366363de71fca0d",
+    "url": "/static/js/runtime-main.ff2a2ca0.js"
+  },
+  {
+    "revision": "5afeebbd533af6c7839356c072d24e79",
+    "url": "/static/media/group.5afeebbd.png"
+  },
+  {
+    "revision": "df8b21e65d300342479c3681f682c8de",
+    "url": "/static/media/stockTable.df8b21e6.png"
+  }
+]);
